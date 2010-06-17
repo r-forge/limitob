5ec0c9584304abdbe0f5bf76ffc65b951d61150e
knguyen (Khanh Nguyen) <nguyen.h.khanh@gmail.com>
2010-06-17 17:59:31 +0000

    more tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/limitob@37 8f7943a7-fdc2-4c49-894c-e33f39cc6ee5

--- kcm.orderbook/tests/read.orders.test.R
+++ kcm.orderbook/tests/read.orders.test.R
@@ -2,12 +2,32 @@ library(kcm.orderbook)
 
 
 #something is wrong. This returns TRUE in R, but fail with test
-#ob <- orderbook(feed="sample.txt")
-#ob <- read.orders(ob, n=20)
+ob <- orderbook(feed="sample.txt")
 
-#load("ob.next.20.RData")
-#stopifnot(
-#	isTRUE(identical(ob.next.20@current.ob,
-#	                 ob@current.ob))
-#)
+# read 20 orders
+ob <- read.orders(ob, n=20)
 
+load("ob.next.20.RData")
+stopifnot(
+	isTRUE(identical(ob.next.20@current.ob,
+	                 ob@current.ob))
+)
+
+# go back 5 orders
+ob <- read.orders(ob, n=-5)
+load("ob.go.back.5.RData")
+stopifnot(
+	isTRUE(identical(ob.go.back.5@current.ob,
+	                 ob@current.ob))
+)
+
+# reset 
+
+ob <- reset(ob)
+ob <- read.orders(ob)
+ob <- view.trade(ob, n=1)
+load("ob.1st.trade.RData")
+stopifnot(
+	isTRUE(identical(ob.1st.trade@current.ob,
+	                 ob@current.ob))
+)

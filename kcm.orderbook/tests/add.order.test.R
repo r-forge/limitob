6250bb0fb84e5f397ee88484377235a5a6565383
knguyen (Khanh Nguyen) <nguyen.h.khanh@gmail.com>
2010-06-17 14:06:09 +0000

    new tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/limitob@31 8f7943a7-fdc2-4c49-894c-e33f39cc6ee5

--- kcm.orderbook/tests/add.order.test.R
+++ kcm.orderbook/tests/add.order.test.R
@@ -2,32 +2,17 @@ library(kcm.orderbook)
 
 load("sample.RData")
 
-load("add.order.test.1.RData") 
+load("add.order.test1.RData") 
 x = add.bid.1@current.ob
-y = add.order(ob, type = "BID", price = 26.80, size = 150)
+y = add.order(ob, type = "BID", price = 11, size = 1590)
 y = y@current.ob
 
 stopifnot(isTRUE(identical(x, y)))
 
 
-load("add.order.test.2.RData")
-x = add.bid.2@current.ob
-y = add.order(ob, type="BID", price=27.22, size=15000)
-y = y@current.ob
-
-stopifnot(isTRUE(identical(x,y)))
-
-
-load("add.order.test.3.RData")
+load("add.order.test2.RData")
 stopifnot(
 	isTRUE(identical(add.ask.1@current.ob,
 	                 add.order(ob, type="ASK", 
-			                price=27.30, size=123456)@current.ob))
-)
-
-load("add.order.test.4.RData")
-stopifnot(
-	isTRUE(identical(add.ask.2@current.ob,
-	                 add.order(ob, type="ASK", 
-			                price=27.60, size=123456)@current.ob))
+			                price=12.10, size=20000)@current.ob))
 )
--- kcm.orderbook/tests/remove.replace.order.test.R
+++ kcm.orderbook/tests/remove.replace.order.test.R
@@ -6,12 +6,12 @@ load("sample.RData")
 load("remove.order.test.1.RData")
 stopifnot(
 	isTRUE(identical(remove.order.1@current.ob,
-	                 remove.order(ob, id=929626)@current.ob))
+	                 remove.order(ob, id=52109)@current.ob))
 )
 
 load("replace.order.test.1.RData")
 stopifnot(
 	isTRUE(identical(replace.order.1@current.ob,
-	                 replace.order(ob, id=473046, size=500)@current.ob))
+	                 replace.order(ob, id=52109, size=500)@current.ob))
 )
 

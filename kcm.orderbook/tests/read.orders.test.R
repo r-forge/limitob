51651f8101b10d50185e79274da457f8edd22101
knguyen (Khanh Nguyen) <nguyen.h.khanh@gmail.com>
2010-06-17 14:32:41 +0000

    read order test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/limitob@34 8f7943a7-fdc2-4c49-894c-e33f39cc6ee5

---
+++ kcm.orderbook/tests/read.orders.test.R
@@ -0,0 +1,13 @@
+library(kcm.orderbook)
+
+
+#something is wrong. This returns TRUE in R, but fail with test
+#ob <- orderbook(feed="sample.txt")
+#ob <- read.orders(ob, n=20)
+
+#load("ob.next.20.RData")
+#stopifnot(
+#	isTRUE(identical(ob.next.20@current.ob,
+#	                 ob@current.ob))
+#)
+

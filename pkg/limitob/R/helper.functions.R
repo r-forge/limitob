e7fe87731e68867d46eef8178d514c27b158cb78
andyliu52 (Andrew Liu) <atl1@williams.edu>
2010-06-24 18:06:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/limitob@84 8f7943a7-fdc2-4c49-894c-e33f39cc6ee5

--- pkg/limitob/R/helper.functions.R
+++ pkg/limitob/R/helper.functions.R
@@ -279,40 +279,7 @@
     time = format(time, format ="%H:%M:%S")
 
 
-    ## Set default settings for the plot.
-    x = read.time(object, time[1])
-    x = .combine.size(x, bounds = 0.1)
-
-    ## Maximum size, max/min price and difference between the max
-    ## and min price for purposes of drawing the axes.
-
-    max.size = max(x[[ob.names[2]]])
-    max.size = ceiling(max.size + max.size/20)
-
-    min.price = signif(min(x[ob.names[[1]]])-.05,3)
-    max.price = round(max(x[ob.names[[1]]])+0.5)
-    midpoint = mid.point(object)
-
-    ## Creating the x axis values.
-
-    x.limits =  list(c(max.size,0), c(0,max.size))
-    x.at = ceiling(seq(0, max.size, max.size/5))
-
-    ## Creating the y axis values.
-
-    tmp.at = formatC(seq(min.price, max.price, .1), format = "f", digits = 2)
-    yask.at = vector()
-    ybid.at = vector()
-
-    for(i in 1:length(tmp.at)){
-  	if(i%%2==0){
-            yask.at[i] = tmp.at[i]
-            ybid.at[i]=""
-  	} else {
-            yask.at[i] = ""
-	   	ybid.at[i] = tmp.at[i]
-   	}
-    }
+    ## Define function for plotting the y-axis values.
 
     new.yscale.components <- function(...) {
         ans <- yscale.components.default(...)
@@ -328,25 +295,60 @@
     for (i in 1:length(time)){
         tmp = read.time(object, time[i])
         x = .combine.size(tmp, bounds = 0.1)
+
+        ## Maximum size, max/min price and difference between the max
+        ## and min price for purposes of drawing the axes.
+
+        max.size = max(x[[ob.names[2]]])
+        max.size = ceiling(max.size + max.size/20)
+
+        min.price = signif(min(x[ob.names[[1]]])-.05,3)
+        max.price = round(max(x[ob.names[[1]]])+0.5)
+        midpoint = mid.point(object)
+
+        ## Creating the x axis values.
+
+        x.limits =  list(c(max.size,0), c(0,max.size))
+        x.at = ceiling(seq(0, max.size, max.size/5))
+
+        ## Creating the y axis values.
+
+        tmp.at = formatC(seq(min.price, max.price, .1), format = "f", digits = 2)
+        yask.at = vector()
+        ybid.at = vector()
+
+        for(j in 1:length(tmp.at)){
+            if(i%%2==0){
+                yask.at[j] = tmp.at[j]
+                ybid.at[j]=""
+            } else {
+                yask.at[j] = ""
+	   	ybid.at[j] = tmp.at[j]
+            }
+        }
+
+        ## Ordering so that Bid comes before Ask
+
         x[[ob.names[3]]] <- ordered(x[[ob.names[3]]], levels = c(ob.names[7],
                                                       ob.names[6]))
-       
-        assign(paste("x", i, sep = "."), 
-						  xyplot(x[[ob.names[1]]]~x[[ob.names[2]]]|x[[ob.names[3]]], data = x,
-                                    ylab = "Price", xlab = "Size (Shares)", main = "Order Book",
-                                    scales = list(x = list(relation = "free",
-                                                  limits = x.limits,
-                                                  at = x.at,
-                                                  axs = "i"),
-                                    y = list(at = tmp.at, alternating = 3)),
-                                    yscale.components = new.yscale.components,
-                                    panel = function(...){
-                                        panel.xyplot(...)
-                                        panel.lines(..., type = "H")
-                                    }
-                                    )
 
-				)
+        assign(paste("x", i, sep = "."),
+               xyplot(x[[ob.names[1]]]~x[[ob.names[2]]]|x[[ob.names[3]]], data = x,
+                      ylab = "Price", xlab = "Size (Shares)", main = paste("Order Book",
+                                                              time[i] , sep = " - "),
+                      scales = list(x = list(relation = "free",
+                                    limits = x.limits,
+                                    at = x.at,
+                                    axs = "i"),
+                      y = list(at = tmp.at, alternating = 3)),
+                      yscale.components = new.yscale.components,
+                      panel = function(...){
+                          panel.xyplot(...)
+                          panel.lines(..., type = "H")
+                      }
+                      )
+
+               )
         object = tmp
     }
 
@@ -355,7 +357,7 @@
     for(i in 1:length(time)){
         x = paste("x", i, sep = ".")
         print(get(x))
-        Sys.sleep(1)
+        Sys.sleep(.25)
         rm(x)
     }
 
